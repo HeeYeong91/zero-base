90db836c525e594334239fa12c8b7350945a6910
Heeyeong <gmldud5659@naver.com>
2024-04-16 23:30:50 +0900

    :memo: Java - 변수와 자료형 학습

--- Part01.java기초/Chapter01.Java프로그래밍/Java_02_2/src/Main.java
+++ Part01.java기초/Chapter01.Java프로그래밍/Java_02_2/src/Main.java
@@ -5,20 +5,55 @@ public class Main {
 
 //      1. 자료형 - 숫자
         System.out.println("== 숫자 ==");
+        
 //      1-1. 정수
+        int intNum = 10; // int 기본타입
+        System.out.println("intNum = " + intNum);
+
+        System.out.println(Integer.MIN_VALUE); // Integer 기본타입보다 기능이 많다
+        System.out.println(Integer.MAX_VALUE); // 대문자 스네이크표기법 (상수 = 변경되지 않는 값)
+
+        int intNum2 = Integer.MAX_VALUE;
+        System.out.println("intNum2 = " + intNum2);
+        int intNum3 = Integer.MAX_VALUE + 1;
+        System.out.println("intNum3 = " + intNum3); // 최대값이 넘어가서 최소값으로 돌아감
+        long longNum = (long) Integer.MAX_VALUE + 1; // long 타입으로 형변환을 해야 제대로 표기됨
 
 //      1-2. 실수
+        float floatNum = 1.23f; // 값 뒤에 f를 안쓰면 double 타입
+        double doubleNum = 1.23;
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
 
 //      1-3. 2진수 / 8진수 / 16진수
+        int numBase2 = 0b1100;
+        System.out.println("numBase2 = " + numBase2); // 10진수로 출력
+        int numBase8 = 014;
+        System.out.println("numBase8 = " + numBase8); // 10진수로 출력
+        int numBase16 = 0xC;
+        System.out.println("numBase16 = " + numBase16); // 10진수로 출력
+
+        System.out.println("0b" + Integer.toBinaryString(numBase2));
+        System.out.println("0" + Integer.toOctalString(numBase8));
+        System.out.println("0x" + Integer.toHexString(numBase16));
 
 
 //      2. 자료형 - 부울
         System.out.println("== 부울 ==");
+        boolean isPass = true;
+        System.out.println("isPass = " + isPass);
+        boolean isOk = false;
+        System.out.println("isOk = " + isOk);
 
 
 //      3. 자료형 - 문자
         System.out.println("== 문자 ==");
-
+        char keyFirst = 'a';
+        System.out.println("keyFirst = " + keyFirst);
+        char keyList = 'z';
+        System.out.println("keyList = " + keyList);
+        System.out.println((int) keyFirst); // int로 형변환
+        System.out.println((int) keyList); // 아스키 코드 값
 
     }
 }
--- Part01.java기초/Chapter01.Java프로그래밍/Java_02_3/src/Main.java
+++ Part01.java기초/Chapter01.Java프로그래밍/Java_02_3/src/Main.java
@@ -1,38 +1,80 @@
 // Java 프로그래밍 - 변수와 자료형_3
 
-import java.util.Locale;
-
 public class Main {
     public static void main(String[] args) {
 
 //      1. 자료형 - 문자열
         System.out.println("== 문자열 ==");
 
+        String s1 = "Hello World!";
+        System.out.println("s1 = " + s1);
+        String s2 = "01234";
+        System.out.println("s2 = " + s2);
 
 //      1-1. equals
+        String s3 = "Hi";
+        String s4 = "Hi";
+        System.out.println(s3.equals(s4));
+        System.out.println(s3 == s4);
 
+        String s5 = new String("Hi");
+        System.out.println(s3.equals(s5)); // true <- 값 비교
+        System.out.println(s3 == s5); // false <- 객체 비교
 
 //      1-2. indexOf
-
-
+        String s6 = "Hello! World!";
+        System.out.println(s6.indexOf("!")); // 5 <- index는 0부터 시작
+        System.out.println(s6.indexOf(s6.indexOf("!") + 1)); // 2번째 ! 찾기
+        
 //      1-3. replace
-
+        String s7 = s6.replace("Hello", "Bye");
+        System.out.println("s7 = " + s7);
 
 //      1-4. substring
-
+        System.out.println(s7.substring(0, 3)); // 3에서 하나 전 까지 출력 0 ~ 2
+        System.out.println(s7.substring(0, s7.indexOf("!") + 1)); // !까지
 
 //      1-5. toUpperCase
-
+        System.out.println(s7.toUpperCase());
 
 
 //      2. 자료형 - StringBuffer
         System.out.println("== StringBuffer ==");
 
+        StringBuffer sb1 = new StringBuffer(); // 데이터가 변경되어도 객체가 새로 생성되지 않는다.
+        sb1.append("01234");
+        System.out.println("sb1 = " + sb1);
+        sb1.append("56789");
+        System.out.println("sb1 = " + sb1);
+
+        String a = "01234";
+        String b = "56789";
+        String bak = a;
+        System.out.println(a == bak); // true
+
+        a += b; // 객체가 새로 생성되고, 이런 경우 메모리 생성이 여러번 된다. 시스템 적으로 딜레이
+        System.out.println(a);
+        System.out.println(a == bak); // false
 
 
 //      3. 자료형 - 배열
         System.out.println("== 배열 ==");
 
+        int[] myArray1 = {1, 2, 3, 4, 5}; // index 0 부터 시작
+        System.out.println(myArray1[0]);
+        System.out.println(myArray1[1]);
+        System.out.println(myArray1[2]);
+        System.out.println(myArray1[3]);
+        System.out.println(myArray1[4]); // index 범위를 벗어나면 에러 발생
+
+        char[] myArray2 = {'a', 'b', 'c', 'd', 'e'}; // 바로 초기화
+        System.out.println(myArray2[2]);
+
+        String[] myArray3 = new String[3];
+        myArray3[0] = "Hello";
+        myArray3[1] = " ";
+        myArray3[2] = "World!";
+        System.out.println(myArray3[0] + myArray3[1] + myArray3[2]);
 
     }
 }
