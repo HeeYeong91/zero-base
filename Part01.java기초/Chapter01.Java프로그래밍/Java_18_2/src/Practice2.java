de33f837eeee28e1bee96521bf4dad025d6b0a89
Heeyeong <gmldud5659@naver.com>
2024-04-27 16:50:47 +0900

    :memo: Java - 연습 문제 풀이_2

---
+++ Part01.java기초/Chapter01.Java프로그래밍/Java_18_2/src/Practice2.java
@@ -0,0 +1,31 @@
+
+public class Practice2 {
+
+    public static String solution(int num){
+        String result = "";
+
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        int i = 0;
+        while (num > 0) {
+            while (num >= values[i]) { // num을 values 값으로 뺄 수 있다면 num에서 빼고 roman 문자를 결과에 더해준다.
+                num -= values[i];
+                result += roman[i];
+            }
+            i++;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+        System.out.println(solution(6));
+        System.out.println(solution(13));
+        System.out.println(solution(26));
+        System.out.println(solution(1994));
+    }
+}
